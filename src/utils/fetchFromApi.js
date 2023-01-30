88d43dccc6e32911985a0b2caf4efa797b03dc72
mohammadOmar <mohammadomarb00@gmail.com>
2023-01-30 10:18:49 +0200

    hide keys

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -1,16 +1,10 @@
 import axios from "axios";
 const Base_Url = "https://youtube-v3-alternative.p.rapidapi.com";
 
-// const options = {
-//   headers: {
-//     "X-RapidAPI-Key": "a24a36beecmshe4496d515331a8fp1e91b1jsncd72d20def5e",
-//     "X-RapidAPI-Host": "youtube-v3-alternative.p.rapidapi.com",
-//   },
-// };
 
 const options = {
   headers: {
-    'X-RapidAPI-Key': 'c5d599bbc7msh8cf969e91c2ce0ap1c6de9jsn8407b75eb18c',
+    'X-RapidAPI-Key': import.meta.process.VITE_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
   }
 };
