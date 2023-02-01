61ec993c7eb4b0400b0bb32a83d10a9a12b753cd
mohammadOmar <mohammadomarb00@gmail.com>
2023-02-01 09:42:25 +0200

    fix channel access from search

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ import {Feed, VideoDetail, ChannelDetail, SearchFeed} from "./components";
 //layout
 import RootLayout from "./layout/RootLayout";
 
-
 const router = createBrowserRouter(
   createRoutesFromChildren(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Feed />} />
       <Route path="video/:id" element={<VideoDetail />} />
-      <Route path="Channel/:id" element={<ChannelDetail />} />
       <Route path="search/:searchTerm" element={<SearchFeed />} />
+      <Route path="search/:searchTerm/channel/:id" element={<ChannelDetail />} />
+      <Route path="channel/:id" element={<ChannelDetail />} />
     </Route>
   )
 );
